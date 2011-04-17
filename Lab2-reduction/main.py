0ae77a5893a43061eecaa665a39ff0bc44a94f2d
Jon Wedaman <jweede@gmail.com>
2011-04-17 11:41:00 -0400

    clean up

--- Lab2-reduction/main.py
+++ Lab2-reduction/main.py
@@ -6,42 +6,40 @@ import pyopencl as cl
 import numpy
 
 class CL:
-    def __init__(self):
+    def __init__(self, n):
         self.ctx = cl.create_some_context()
-        self.queue = cl.CommandQueue(self.ctx)
+        cf = cl.command_queue_properties
+        self.queue = cl.CommandQueue(self.ctx, properties=cf.PROFILING_ENABLE)
+        self.n = n
 
     def loadProgram(self, filename):
         #read in the OpenCL source file as a string
-        f = open(filename, 'r')
-        fstr = "".join(f.readlines())
-        print fstr
+        fstr = open(filename).read()
+        kernel_params = {"max_n": self.n}
+
         #create the program
-        self.program = cl.Program(self.ctx, fstr).build()
+        self.program = cl.Program(self.ctx, fstr % kernel_params).build()
 
     def popCorn(self):
         mf = cl.mem_flags
 
         #initialize client side (CPU) arrays
-        self.a = numpy.array(range(10), dtype=numpy.int32)
-        self.b = numpy.array(range(10), dtype=numpy.int32)
+        self.a = numpy.array(range(self.n), dtype=numpy.int32)
 
         #create OpenCL buffers
-        self.a_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.a)
-        self.b_buf = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=self.b)
-        self.dest_buf = cl.Buffer(self.ctx, mf.WRITE_ONLY, self.b.nbytes)
+        self.a_buf = cl.Buffer(self.ctx, mf.READ_WRITE | mf.COPY_HOST_PTR, hostbuf=self.a)
 
     def execute(self):
-        self.program.part1(self.queue, self.a.shape, None, self.a_buf, self.b_buf, self.dest_buf)
-        c = numpy.empty_like(self.a)
-        cl.enqueue_read_buffer(self.queue, self.dest_buf, c).wait()
+        event = self.program.part1(self.queue, self.a.shape, (256,), self.a_buf )
+        #event = self.program.part1(self.queue, self.a.shape, None, self.a_buf)
+        cl.enqueue_read_buffer(self.queue, self.a_buf, self.a).wait()
+        print 1e-9 * (event.profile.end - event.profile.start)
         print "a", self.a
-        print "b", self.b
-        print "c", c
 
 
 
 if __name__ == "__main__":
-    example = CL()
+    example = CL(2**22)
     example.loadProgram("part1.cl")
     example.popCorn()
     example.execute()
