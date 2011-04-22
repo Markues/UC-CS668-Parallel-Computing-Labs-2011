08e23c088fc6bf663b517fa11ca6082138dcd955
Jon Wedaman <jweede@gmail.com>
2011-04-22 16:15:00 -0400

    partial modification.
    passing to jeremy

--- Lab2-reduction/main.py
+++ Lab2-reduction/main.py
@@ -11,6 +11,7 @@ class CL:
         cf = cl.command_queue_properties
         self.queue = cl.CommandQueue(self.ctx, properties=cf.PROFILING_ENABLE)
         self.n = n
+        self.primes = []
 
     def loadProgram(self, filename):
         #read in the OpenCL source file as a string
@@ -31,10 +32,20 @@ class CL:
 
     def execute(self):
         event1 = self.program.sieve(self.queue, (self.n,), None, self.a_buf)
+        cl.enqueue_read_buffer(self.queue, self.a_buf, self.a).wait()
         # store bit mask of primes as integers
+        for i,x in enumerate(self.a):
+            if x:
+                self.primes.append(i)
+
+        self.a = numpy.ones((self.n, 1), dtype=numpy.uint32)
+        
+        for x in self.primes:
+                self.a[0][x % ] = 0
+
         # send integers and new bit mask to pfilter
         event2 = self.program.pfilter(self.queue, (self.n,), None, self.a_buf)
-        cl.enqueue_read_buffer(self.queue, self.a_buf, self.a).wait()
+        
         print 'Sieve Duration:', 1e-9 * (event1.profile.end - event1.profile.start)
         print 'Filter Duration:', 1e-9 * (event2.profile.end - event2.profile.start)
         #print [i for i in self.a if not i]
