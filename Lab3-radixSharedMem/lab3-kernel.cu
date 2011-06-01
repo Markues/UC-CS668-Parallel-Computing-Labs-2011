3c64c8ee0ecd46e6cf7bd692dd0c03cad5cabc0a
Alex Padgett <alex.p@fuse.net>
2011-06-01 15:33:25 -0400

    Added a second scan method that gets the 1's and 0's array for prefix sum, just need to implement a parallel prefix sum operation

--- Lab3-radixSharedMem/lab3-kernel.cu
+++ Lab3-radixSharedMem/lab3-kernel.cu
@@ -53,7 +53,7 @@ __device__ static void radixSort(int *A, int *C, int N, int bit)
 		}
 	}
 }
-__global__ void bigscan(int *g_odata, int *g_idata,int *C, int *f, int n, int bit)
+__global__ void bigscan2(int *g_idata,int *C, int *f, int n, int bit)
 {
     // Dynamically allocated shared memory for scan kernels
     extern  __shared__  int temp[];
@@ -67,60 +67,130 @@ __global__ void bigscan(int *g_odata, int *g_idata,int *C, int *f, int n, int bi
 	temp[2*thid+1] = 1-((int)(g_idata[2*thid+1]/pow(2.0,bit)) % 2);
 
 	// build the sum in place up the tree
-	for (int d = n>>1; d > 0; d >>= 1)
-	{
-		__syncthreads();
+    for (int d = n>>1; d > 0; d >>= 1)
+    {
+        __syncthreads();
 
-		if (thid < d)      
-		{
-			int ai = offset*(2*thid+1)-1;
-			int bi = offset*(2*thid+2)-1;
+        if (thid < d)      
+        {
+            int ai = offset*(2*thid+1)-1;
+            int bi = offset*(2*thid+2)-1;
 
-			temp[bi] += temp[ai];
-		}
+            temp[bi] += temp[ai];
+        }
 
-		offset *= 2;
-	}
+        offset *= 2;
+    }
 
-	// scan back down the tree
+    // scan back down the tree
 
-	// clear the last element
-	if (thid == 0)
-	{
-		temp[n - 1] = 0;
-	}   
+    // clear the last element
+    if (thid == 0)
+    {
+        temp[n - 1] = 0;
+    }   
 
-	// traverse down the tree building the scan in place
-	for (int d = 1; d < n; d *= 2)
-	{
-		offset >>= 1;
-		__syncthreads();
+    // traverse down the tree building the scan in place
+    for (int d = 1; d < n; d *= 2)
+    {
+        offset >>= 1;
+        __syncthreads();
 
-		if (thid < d)
-		{
-			int ai = offset*(2*thid+1)-1;
-			int bi = offset*(2*thid+2)-1;
+        if (thid < d)
+        {
+            int ai = offset*(2*thid+1)-1;
+            int bi = offset*(2*thid+2)-1;
 
-			float t   = temp[ai];
-			temp[ai]  = temp[bi];
-			temp[bi] += t;
-		}
-	}
-
-	__syncthreads();
-
-	// write results to global memory
-	g_odata[2*thid]   = temp[2*thid];
-	g_odata[2*thid+1] = temp[2*thid+1];
+            float t   = temp[ai];
+            temp[ai]  = temp[bi];
+            temp[bi] += t;
+        }
+    }
 
 	__syncthreads();
 	radixSort(g_idata,C,n, bit);
 }
 
+__device__ void bigscan(int *g_odata, int *g_idata,int *C, int *f, int n, int bit)
+{
+    // Dynamically allocated shared memory for scan kernels
+    extern  __shared__  int temp[];
+    int thidx = 4*threadIdx.x;
+	
+	for(int thid = thidx;thid < thidx + 4; thid++){
+		int offset = 1;
+		// Cache the computational window in shared memory
+		temp[thid]   = 1-((int)(g_idata[thid]/pow(2.0,bit)) % 2);
+		temp[thid+1] = 1-((int)(g_idata[thid+1]/pow(2.0,bit)) % 2);
+
+		// build the sum in place up the tree
+		for (int d = n>>1; d > 0; d >>= 1)
+		{
+			__syncthreads();
+
+			if (thid < d)      
+			{
+				int ai = offset*(thid+1)-1;
+				int bi = offset*(thid+2)-1;
+
+				temp[bi] += temp[ai];
+			}
+
+			offset *= 2;
+		}
+
+		// scan back down the tree
+
+		// clear the last element
+		if (thid == 0)
+		{
+			temp[n - 1] = 0;
+		}   
+
+		// traverse down the tree building the scan in place
+		for (int d = 1; d < n; d *= 2)
+		{
+			offset >>= 1;
+			__syncthreads();
+
+			if (thid < d)
+			{
+				int ai = offset*(thid+1)-1;
+				int bi = offset*(thid+2)-1;
+
+				float t   = temp[ai];
+				temp[ai]  = temp[bi];
+				temp[bi] += t;
+			}
+		}
+		g_odata[thid] = temp[thid];
+		g_odata[thid+1] = temp[thid+1];
+	}
+	
+
+	__syncthreads();
+	radixSort(g_idata,C,n, bit);
+}
+__global__ void scan(int *g_odata, int *g_idata, int *C, int *f, int n, int bit)
+{
+	extern __shared__ int temp[]; // allocated on invocation
+	int thidx = 8*threadIdx.x;
+	
+	// Load input into shared memory.
+	// This is exclusive scan, so shift right by one
+	// and set first element to 0
+	for(int thid = thidx;thid < thidx + 8; thid++){
+		temp[thid] = (thid > 0) ? (((int)(g_idata[thid-1]/pow(2.0,bit)) % 2)) : 0;	
+		__syncthreads();
+		g_odata[thid] = temp[thid];	
+	}
+	//bigscan(g_odata,g_idata,C,f,n,bit);
+}
+
 int main(int argc, char* argv[])
 {
 	int  *c, *a, *d_a, *d_c, *d_prefix, *h_prefix, *h_f, *d_f; //host and device arrays
-	const int n = 1024; // num elements in array
+	const int n = 4000; // num elements in array
 	size_t size = n * sizeof(int); //size of array
 	
 	a = (int *)malloc(size);//allocate host array
@@ -133,31 +203,37 @@ int main(int argc, char* argv[])
 	cudaMalloc((void**) &d_f, size); //allocate device array
 
 	//init host array
-	for(int i = 0; i < n;i++) a[i] = (int)rand() % 4294967297;	
+	for(int i = 0; i < n;i++) a[i] = (int)rand() % 10;//4294967297;	
 	printf(" OLD LIST: \n");
 	for(int k=0;k < n;++k) printf("%d ",a[k]);
 
 	//copy it to the device 
 	
 	
-	for(int bit = 0; bit <= 32;bit++){ //for each bit
+	for(int bit = 0; bit <= 0;bit++){ //for each bit
 		cudaMemcpy(d_a,a,size,cudaMemcpyHostToDevice);
 		cudaMemcpy(d_c,c,size,cudaMemcpyHostToDevice);
 		cudaMemcpy(d_prefix,h_prefix,size,cudaMemcpyHostToDevice);
 		cudaMemcpy(d_f,h_f,size,cudaMemcpyHostToDevice);
-		
-		bigscan<<<1,512,size>>>(d_prefix,d_a,d_c,d_f,n,bit);
-		//radixSort<<<1,512,size>>>(d_c,d_a, d_prefix, d_f, n);		
+		scan<<<1,512,size>>>(d_prefix,d_a,d_c,d_f,n,bit);
+		//bigscan<<<1,512,size>>>(d_prefix,d_a,d_c,d_f,n,bit);
 		cudaMemcpy(h_prefix, d_prefix, sizeof(int) * n, cudaMemcpyDeviceToHost);
 		cudaMemcpy(h_f, d_f, sizeof(int) * n, cudaMemcpyDeviceToHost);
 		cudaMemcpy(a, d_c, sizeof(int) * n, cudaMemcpyDeviceToHost);		
 	}
-
 	printf("\nPREFIX: \n");
-	for(int k=0;k < n;++k) printf("%d ",h_prefix[k]);
-	printf("\nORDERED LIST: \n");
+	for(int k=0;k < n/2;++k) printf("%d ",h_prefix[k]);
+	//printf("\nORDERED LIST: \n");
 	
-	for(int k=0;k < n;++k) printf("%d ",a[k]);
+	//for(int k=0;k < n;++k) printf("%d ",a[k]);
+	/*bool inOrder = true;
+	for(int k=0;k < n;++k) if(a[k] < a[k-1]){inOrder = false;}
+	if(inOrder == true){
+		printf("\n\n\nIN ORDER!");
+	}
+	else{
+		printf("NOT IN ORDER!");
+	}*/
 	cudaFree(c);
 	free(a); 
 	return 0;
